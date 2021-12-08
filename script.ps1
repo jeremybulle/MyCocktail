73345ec3d5d90284cba2e00609e8951e14ef6c28
jeremybulle <jeremyjp.bulle@gmail.com>
2021-12-08 13:31:53 +0100

    try other param for sonar scan

--- script.ps1
+++ script.ps1
@@ -38,3 +38,25 @@ function MoveCoverageFiles{
 # $testFile = Get-Item ./TestDir\Test.txt
 # Rename-Item $testFile -NewName "$($testFile.BaseName)-Renamed.txt"
 
+# - name : run tests and regroup coverage files
+#         shell: powershell
+#         run: |
+#           dotnet test --collect:"XPlat Code Coverage" -p:CollectCoverage=true -p:CoverletOutputFormat=opencover
+
+#           $CoverageFiles = Get-ChildItem -Path *\TestResults\*\coverage.cobertura.xml -Recurse
+
+#           foreach ($file in $CoverageFiles) {
+#             #extract id
+#             $path = Split-Path -Path $file -Parent
+#             $path = $path.Split("\")
+#             $id = $path[$path.Count -1]
+
+#             #move file
+#             Move-Item -Path $file.FullName -Destination ./CoverageFiles
+            
+#             #retrieve file moved
+#             $movedFile = Get-Item ./CoverageFiles\coverage.cobertura.xml
+
+#             #rename file
+#             Rename-Item $movedFile -NewName "$($id).$($movedFile.Name)"
+#           }
