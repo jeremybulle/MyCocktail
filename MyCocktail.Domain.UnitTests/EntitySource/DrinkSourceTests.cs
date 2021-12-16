51545b7659f58f3133edd6f1595f92250b3bb3ee
jeremybulle <jeremyjp.bulle@gmail.com>
2021-12-16 17:09:27 +0100

    TU DrinkSource

---
+++ MyCocktail.Domain.UnitTests/EntitySource/DrinkSourceTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using MyCocktail.Domain.EntitySource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyCocktail.Domain.UnitTests.EntitySource
+{
+    public class DrinkSourceTests
+    {
+
+        private readonly Fixture _fixture;
+
+        public DrinkSourceTests()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public void Constructor_WhenValidParameters_ShouldReturnDrinkSource()
+        {
+            //Arrange
+
+            //Act
+            var drinkSource = _fixture.Create<DrinkSource>();
+
+            //Assert
+            Assert.NotNull(drinkSource);
+            drinkSource.Should().BeOfType<DrinkSource>();
+        }
+
+        [Fact]
+        public void Constructor_WhenValidParameters_ShouldReturnDrinksSource()
+        {
+            //Arrange
+
+            //Act
+            var drinkSource = _fixture.Create<DrinksSource>();
+
+            //Assert
+            Assert.NotNull(drinkSource);
+            drinkSource.Should().BeOfType<DrinksSource>();
+        }
+    }
+}
